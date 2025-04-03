b23c334fb8bebd757f96d2f06d9f27d3d16673a2
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-03 23:33:55 +0500

    creating portfolio for my self

---
+++ src/pages/about.jsx
@@ -0,0 +1,16 @@
+import Summery from '../components/sections/summery'
+import Resume from '../components/sections/resume'
+import Testimonials from '../components/sections/testimonials'
+
+const About = () => {
+    return (
+        <>
+            <Summery />
+            <Resume />
+            <Testimonials />
+           
+        </>
+    )
+}
+
+export default About
---
+++ src/utlits/fackData/projectData.jsx
@@ -0,0 +1,60 @@
+import bytvance from "../../assets/images/projects/bytvance.png"
+import abita from "../../assets/images/projects/abita.png"
+import hoobank from "../../assets/images/projects/hoobank.png"
+import mysticove from "../../assets/images/projects/mysticove.png"
+import passwordgenerator from "../../assets/images/projects/passwordgenerator.jpg"
+import Stopwatch from "../../assets/images/projects/Stopwatch.jpg"
+import weatherapp from "../../assets/images/projects/weatherapp.jpg"
+import stats from "../../assets/images/projects/stats.png"
+
+export const projectsData = [
+    {
+        id: 1,
+        src: bytvance,
+        category: "MERN",
+        title: "Mobile Application Design",
+    },
+    {
+        id: 2,
+        src: abita,
+        category: "React",
+        title: "Website Makeup Design",
+    },
+    {
+        id: 3,
+        src: hoobank,
+        category: "Landing page",
+        title: "Brand Identity and Motion Design",
+    },
+    {
+        id: 4,
+        src: mysticove,
+        category: "Marketing",
+        title: "Mobile Application Design",
+    },
+    {
+        id: 5,
+        src: passwordgenerator,
+        category: "Design",
+        title: "Brand Identity and Motion Design",
+    },
+    {
+        id: 6,
+        src: Stopwatch,
+        category: "Marketing",
+        title: "Brand Identity and Motion Design"
+    },
+    {
+        id: 7,
+        src: weatherapp,
+        category: "Marketing",
+        title: "Brand Identity and Motion Design"
+    },
+    {
+        id: 8,
+        src: stats,
+        category: "Marketing",
+        title: "Brand Identity and Motion Design"
+    },
+
+]
