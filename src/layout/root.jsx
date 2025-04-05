caac5e4234b3d70fcb3f24c903593700443ed819
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-05 21:07:55 +0500

    set the project data

--- src/layout/root.jsx
+++ src/layout/root.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/sections/header'
 import Footer from '../components/sections/footer'
 import CallToAction from '../components/sections/callToAction'
 import Preloader from '../components/ui/preloader'
+import ScrollToTop from '../components/ScrollToTop'
 
 const RootLayout = () => {
     return (
         <>
             <Preloader />
+            <ScrollToTop/>
             <Header />
             <Outlet />
             <CallToAction />
