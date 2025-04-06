af694686a6beabca9b4e75ae1e2abb783e731304
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-06 18:00:10 +0500

    fix the header

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
                 <div className="navbar-header">
                   <div className="mobile-logo">
                     <Link to="/">
-                    <img src={isDarkMode ? darkLogo : lightLogo} alt="Logo" title="Logo" />
+                      <img src={isDarkMode ? darkLogo : lightLogo} alt="Logo" title="Logo" />
                     </Link>
                   </div>
                 </div>
@@ -63,35 +63,43 @@ const Header = () => {
                         <Link to={path} className="nav-link-click">{label}</Link>
                       </li>
                     ))}
+                    {/* Hire Me button for mobile */}
+                    <li className="d-lg-none px-3 mt-3">
+                      <Link to="/contact" className="theme-btn w-100">
+                        Hire Me <RiShakeHandsLine size={15} />
+                      </Link>
+                    </li>
+
                   </ul>
                 </div>
               </nav>
             </div>
             <div className="menu-btns d-flex align-items-center gap-3">
-                
-  <DarkModeSwitch
-    className="swtich"
-    checked={isDarkMode}
-    onChange={() => dispatch(toggleDarkMode())}
-    size={25}
-  />
-  <Link to="/contact" className="theme-btn">
-    Hire Me <RiShakeHandsLine size={15} />
-  </Link>
 
 
-  {/* ✅ Hamburger menu placed here for small screens only */}
-  <button
-    type="button"
-    className="navbar-toggle d-block d-lg-none"
-    data-bs-toggle="collapse"
-    data-bs-target=".navbar-collapse"
-  >
-    <span className="icon-bar"></span>
-    <span className="icon-bar"></span>
-    <span className="icon-bar"></span>
-  </button>
-</div>
+            <Link to="/contact" className="theme-btn d-none d-lg-block">
+  Hire Me <RiShakeHandsLine size={15} />
+</Link>
+
+
+              <DarkModeSwitch
+                className="swtich"
+                checked={isDarkMode}
+                onChange={() => dispatch(toggleDarkMode())}
+                size={25}
+              />
+              {/* ✅ Hamburger menu placed here for small screens only */}
+              <button
+                type="button"
+                className="navbar-toggle d-block d-lg-none"
+                data-bs-toggle="collapse"
+                data-bs-target=".navbar-collapse"
+              >
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+            </div>
 
           </div>
         </div>
