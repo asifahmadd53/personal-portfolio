c91cc33ecea6c1b277ad1acf3945b6759a50a419
Muhammad Asif <asifahmadd53@gmail.com>
2026-06-02 18:59:36 +0500

    fix the SEO issue

--- src/pages/single-project.jsx
+++ src/pages/single-project.jsx
@@ -1,4 +1,3 @@
-
 import singleProjectImg from "../assets/images/projects/single-project.jpg"
 import singleProject1 from "../assets/images/projects/single-project1.jpg"
 import singleProject2 from "../assets/images/projects/single-project2.jpg"
